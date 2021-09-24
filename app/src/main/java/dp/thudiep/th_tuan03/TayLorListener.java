36637eefb2f4412e858589a92a18e56d3c16f084
DiepKinh <73871050+DiepKinh@users.noreply.github.com>
2021-09-25 03:56:24 +0700

    Bài 2

---
+++ app/src/main/java/dp/thudiep/th_tuan03/TayLorListener.java
@@ -0,0 +1,7 @@
+package dp.thudiep.th_tuan03;
+
+import android.widget.ImageView;
+
+public interface TayLorListener {
+    void ItemClick(Taylor taylor, ImageView img);
+}
---
+++ app/src/main/java/dp/thudiep/th_tuan03/Taylor.java
@@ -0,0 +1,38 @@
+package dp.thudiep.th_tuan03;
+
+import java.io.Serializable;
+
+public class Taylor implements Serializable {
+    private int anh;
+    private String name, song;
+
+    public Taylor(int anh, String name, String song) {
+        this.anh = anh;
+        this.name = name;
+        this.song = song;
+    }
+
+    public int getAnh() {
+        return anh;
+    }
+
+    public void setAnh(int anh) {
+        this.anh = anh;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+}
